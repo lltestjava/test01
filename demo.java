a730e183cd7c2ee65805aafea30f749288eae4e0
lltestjava <74459943+lltestjava@users.noreply.github.com>
2020-11-15 22:14:03 +0800

    上传的文件
    第一次上传文件，测试一波

---
+++ demo.java
@@ -0,0 +1,14 @@
+package com.designTest.zhuangshizhe.newTest;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person = new PersonImpl();
+		
+		zhuangshizhelei zhuangshilei = new jutigongnenglei(person);
+		zhuangshilei.wear();
+		
+	}
+
+}
